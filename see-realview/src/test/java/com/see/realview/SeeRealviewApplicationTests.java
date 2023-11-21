4f40fddf39fb07277ded11c72f1c98d94e200371
seokwns <seokjun0915@icloud.com>
2023-11-21 22:39:16 +0900

    fix: test environment variables error
    - 테스트 환경에서 외부 변수 주입 에러 해결

--- see-realview/src/test/java/com/see/realview/SeeRealviewApplicationTests.java
+++ see-realview/src/test/java/com/see/realview/SeeRealviewApplicationTests.java
@@ -2,7 +2,15 @@ package com.see.realview;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.TestPropertySource;
 
+@TestPropertySource(properties = {
+        "api.search.kakao.key=testKey",
+        "api.search.naver.id=testId",
+        "api.search.naver.secret:testSecret",
+        "security.jwt.secret.access=a11111111111111111111a11111111111111111111a11111111111111111111a11111111111111111111",
+        "security.jwt.secret.refresh=a11111111111111111111a11111111111111111111a11111111111111111111a11111111111111111111"
+})
 @SpringBootTest
 class SeeRealviewApplicationTests {
 
