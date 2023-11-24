881b7bedc80d4b7404dd2bf822e53991d16b78a2
seokwns <seokjun0915@icloud.com>
2023-11-24 10:16:56 +0900

    config: add github ci/cd variables
    - 깃허브 CI/CD에서 환경변수 등록

--- see-realview/src/test/java/com/see/realview/SeeRealviewApplicationTests.java
+++ see-realview/src/test/java/com/see/realview/SeeRealviewApplicationTests.java
@@ -9,7 +9,8 @@ import org.springframework.test.context.TestPropertySource;
         "api.search.naver.id=testId",
         "api.search.naver.secret:testSecret",
         "security.jwt.secret.access=a11111111111111111111a11111111111111111111a11111111111111111111a11111111111111111111",
-        "security.jwt.secret.refresh=a11111111111111111111a11111111111111111111a11111111111111111111a11111111111111111111"
+        "security.jwt.secret.refresh=a11111111111111111111a11111111111111111111a11111111111111111111a11111111111111111111",
+        "api.google.key=1111111111111111111111111111111111111111111111111"
 })
 @SpringBootTest
 class SeeRealviewApplicationTests {
