04093a3cc42b3be3201fdf0de0a1f03993369c32
seokwns <seokjun0915@icloud.com>
2023-11-29 23:08:30 +0900

    feat: add base data initializer object
    - 초기 데이터 주입을 위한 객체 생성
    	- 이후 이미지 캐시 및 데이터베이스 내용 추가 로직을 작성해야 한다

---
+++ see-realview/src/main/java/com/see/realview/_core/utils/DataGenerator.java
@@ -0,0 +1,26 @@
+package com.see.realview._core.utils;
+
+import com.see.realview.image.repository.ParsedImageRepositoryImpl;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.ApplicationArguments;
+import org.springframework.boot.ApplicationRunner;
+import org.springframework.stereotype.Component;
+
+@Component
+public class DataGenerator implements ApplicationRunner {
+
+    private final ParsedImageRepositoryImpl parsedImageRepository;
+
+
+    public DataGenerator(@Autowired ParsedImageRepositoryImpl parsedImageRepository) {
+        this.parsedImageRepository = parsedImageRepository;
+    }
+
+    @Override
+    public void run(ApplicationArguments args) throws Exception {
+        insertParseImageData();
+    }
+
+    private void insertParseImageData() {
+    }
+}
