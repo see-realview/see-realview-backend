e1a909341bbd07004364f59a1d29e09555a7883d
seokwns <seokjun0915@icloud.com>
2023-12-31 11:21:24 +0900

    feat: change backend api
    - 백엔드 호출 api 변경

--- see-realview/src/main/java/com/see/realview/search/controller/SearchController.java
+++ see-realview/src/main/java/com/see/realview/search/controller/SearchController.java
@@ -14,7 +14,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping("/search")
+@RequestMapping("/api/search")
 public class SearchController {
 
     private final NaverSearcher naverSearcher;
