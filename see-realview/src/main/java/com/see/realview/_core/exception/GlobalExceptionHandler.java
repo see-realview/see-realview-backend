f4c92d161949dc1048d010fb8eb958926a8e6d82
seokwns <seokjun0915@icloud.com>
2024-01-02 19:21:45 +0900

    fix: add invalid method argument exception handler
    - 형식에 맞지 않는 요청이 들어올 경우의 에러 메시지 형태 변경

--- see-realview/src/main/java/com/see/realview/_core/exception/GlobalExceptionHandler.java
+++ see-realview/src/main/java/com/see/realview/_core/exception/GlobalExceptionHandler.java
@@ -5,12 +5,17 @@ import com.see.realview._core.exception.client.ForbiddenException;
 import com.see.realview._core.exception.client.NotFoundException;
 import com.see.realview._core.exception.client.UnauthorizedException;
 import com.see.realview._core.exception.server.ServerException;
+import com.see.realview._core.response.ErrorData;
 import com.see.realview._core.response.Response;
 import com.see.realview._core.response.ResponseData;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.validation.ObjectError;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+import java.util.List;
 
 @RestControllerAdvice
 public class GlobalExceptionHandler {
@@ -44,6 +49,18 @@ public class GlobalExceptionHandler {
     @ExceptionHandler(ServerException.class)
     public ResponseEntity<?> serverError(ServerException exception) {
         return createExceptionResponseData(exception);
+    }
+
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    public ResponseEntity<?> methodArgumentNotValid(MethodArgumentNotValidException exception) {
+        String message = exception.getAllErrors().get(0).getDefaultMessage();
+        int code = ExceptionStatus.INVALID_METHOD_ARGUMENTS_ERROR.getCode();
+
+        ErrorData errorData = new ErrorData(code, message);
+        ResponseData<?> responseData = new ResponseData<>(false, null, errorData);
+
+        HttpStatus status = HttpStatus.BAD_REQUEST;
+        return new ResponseEntity<>(responseData, status);
     }
 
     @ExceptionHandler(Exception.class)
