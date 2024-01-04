6cb1d52c7a0ef52577da87c3c47f1df9bd44dc36
seokwns <seokjun0915@icloud.com>
2024-01-04 09:44:01 +0900

    feat: delete unnecessary constants
    - 불필요한 변수 삭제

--- see-realview/src/main/java/com/see/realview/report/service/ReportService.java
+++ see-realview/src/main/java/com/see/realview/report/service/ReportService.java
@@ -5,8 +5,5 @@ import com.see.realview.report.service.constant.ReportType;
 
 public interface ReportService {
 
-    String BUG_REPORT_PREFIX = "[see-realview][BUG]";
-
-
     void send(ReportType type, ReportRequest request);
 }
