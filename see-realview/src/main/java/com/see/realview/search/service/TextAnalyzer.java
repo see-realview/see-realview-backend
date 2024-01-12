fcc877a5c086a3a13ee3fe83f322564faf335ff3
seokwns <seokjun0915@icloud.com>
2024-01-12 20:54:40 +0900

    feat: enhance text analyzing method
    - 텍스트가 광고인지 분석하는 로직 강화

--- see-realview/src/main/java/com/see/realview/search/service/TextAnalyzer.java
+++ see-realview/src/main/java/com/see/realview/search/service/TextAnalyzer.java
@@ -6,6 +6,7 @@ import org.springframework.stereotype.Component;
 public class TextAnalyzer {
 
     public Boolean analyzePostText(String text) {
+        text = text.trim();
         return (text.contains("제공받") || text.contains("지원받")) ||
                 (
                         (text.contains("업체") || text.contains("체험단") || text.contains("원고료") || text.contains("서비스"))
@@ -15,7 +16,16 @@ public class TextAnalyzer {
     }
 
     public Boolean analyzeImageText(String text) {
-        return (text != null) &&
-                (text.contains("지원받") || text.contains("제공받") || text.contains("협찬"));
+        if (text == null) {
+            return false;
+        }
+
+        text = text.trim();
+        return (text.contains("제공받") || text.contains("지원받")) || text.contains("협찬받") ||
+                (
+                        (text.contains("업체") || text.contains("체험단") || text.contains("원고료") || text.contains("서비스"))
+                                && (text.contains("지원") || text.contains("제공"))
+                                && text.contains("받아")
+                );
     }
 }
