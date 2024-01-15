6dcdffe639cebf3afd72a6f5a0803e7bd6f63a5d
seokwns <seokjun0915@icloud.com>
2024-01-15 21:30:20 +0900

    refactor: separating url into variables
    - URL 및 기타 내용을 변수로 분리

--- see-realview/src/main/java/com/see/realview/_core/config/WebClientConfig.java
+++ see-realview/src/main/java/com/see/realview/_core/config/WebClientConfig.java
@@ -18,6 +18,9 @@ public class WebClientConfig {
     @Value("${api.search.naver.url}")
     private String REQUEST_URL;
 
+    private final static String GOOGLE_VISION_API_URL = "https://vision.googleapis.com/v1/images:annotate";
+    private final static String USER_AGENT = "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/120.0.0.0 Safari/537.36";
+
 
     @Bean(name = "naverWebClient")
     public WebClient naverWebClient() {
@@ -34,7 +37,7 @@ public class WebClientConfig {
     @Bean(name = "googleWebClient")
     public WebClient googleWebClient() {
         return WebClient.builder()
-                .baseUrl("https://vision.googleapis.com/v1/images:annotate")
+                .baseUrl(GOOGLE_VISION_API_URL)
                 .defaultHeader("Content-Type", "application/json; charset=utf-8")
                 .exchangeStrategies(ExchangeStrategies.builder()
                         .codecs(clientCodecConfigurer -> clientCodecConfigurer.defaultCodecs().maxInMemorySize(100 * 1024 * 1024))
@@ -45,7 +48,7 @@ public class WebClientConfig {
     @Bean(name = "imageWebClient")
     public WebClient imageWebClient() {
         return WebClient.builder()
-                .defaultHeader("User-Agent", "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/120.0.0.0 Safari/537.36")
+                .defaultHeader("User-Agent", USER_AGENT)
                 .exchangeStrategies(ExchangeStrategies.builder()
                         .codecs(clientCodecConfigurer -> clientCodecConfigurer.defaultCodecs().maxInMemorySize(100 * 1024 * 1024))
                         .build())
