56e644bc2f53b41629d93be8246675768a5418ec
seokwns <seokjun0915@icloud.com>
2024-01-18 13:29:12 +0900

    feat: implement data initializer
    - 초기 어플리케이션 구동 시 데이터 초기화하는 컴포넌트 구현

--- see-realview/src/main/java/com/see/realview/_core/utils/DataGenerator.java
+++ see-realview/src/main/java/com/see/realview/_core/utils/DataGenerator.java
@@ -1,26 +1,28 @@
 package com.see.realview._core.utils;
 
-import com.see.realview.image.repository.impl.ParsedImageRepositoryImpl;
+import com.see.realview.image.service.ParsedImageService;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.ApplicationArguments;
 import org.springframework.boot.ApplicationRunner;
 import org.springframework.stereotype.Component;
 
 @Component
+@Slf4j
 public class DataGenerator implements ApplicationRunner {
 
-    private final ParsedImageRepositoryImpl parsedImageRepository;
+    private final ParsedImageService parsedImageService;
 
 
-    public DataGenerator(@Autowired ParsedImageRepositoryImpl parsedImageRepository) {
-        this.parsedImageRepository = parsedImageRepository;
+    public DataGenerator(@Autowired ParsedImageService parsedImageService) {
+        this.parsedImageService = parsedImageService;
     }
 
     @Override
-    public void run(ApplicationArguments args) throws Exception {
-        insertParseImageData();
-    }
-
-    private void insertParseImageData() {
+    public void run(ApplicationArguments args) {
+        log.debug("어플리케이션 초기 데이터 생성 시작");
+        parsedImageService.rebase();
+        parsedImageService.rebaseWebDatabase();
+        log.debug("어플리케이션 초기 데이터 생성 완료");
     }
 }
